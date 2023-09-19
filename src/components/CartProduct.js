7f1b3f0a19cb872bc5429da278fecc393f0b3a4b
sambilokonga <abebawbegna@gmail.com>
2023-09-19 17:53:57 +0300

    animation

--- src/components/CartProduct.js
+++ src/components/CartProduct.js
@@ -2,6 +2,7 @@ import Button from "react-bootstrap/Button";
 import { CartContext } from "../CartContext";
 import { useContext } from "react";
 import { getProductData } from "../ProductStore";
+import Fade from "react-reveal/Fade";
 
 function CartProduct(props) {
   const cart = useContext(CartContext);
@@ -11,14 +12,16 @@ function CartProduct(props) {
 
   return (
     <>
-      <h3>{productData.title}</h3>
-      <img src={productData.imgUrl} height="200px" alt="" />
-      <p>{quantity} total</p>
-      <p>${(quantity * productData.price).toFixed(2)}</p>
-      <Button size="sm" onClick={() => cart.deleteFromCart(id)}>
-        Remove
-      </Button>
-      <hr></hr>
+      <Fade left cascade>
+        <h3>{productData.title}</h3>
+        <img src={productData.imgUrl} height="200px" alt="" />
+        <p>{quantity} total</p>
+        <p>${(quantity * productData.price).toFixed(2)}</p>
+        <Button size="sm" onClick={() => cart.deleteFromCart(id)}>
+          Remove
+        </Button>
+        <hr></hr>
+      </Fade>
     </>
   );
 }
